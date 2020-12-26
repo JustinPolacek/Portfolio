6df676e1df267c2c405ab5add1e1ec61b5e32be0
justin Polacek <justinpolacek@gmail.com>
2020-12-25 19:32:22 -0800

    new values

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+
+// WAVE DRIP ANIMATION SCROLL
+
+function scrollAppear(){
+  var curvedText = document.querySelector('.curved-text');
+  var icons = document.querySelector('.icons');
+  var introPosition = curvedText.getBoundingClientRect().top;
+  var introPosition = icons.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 1.6;
+  if(introPosition < screenPosition){
+      curvedText.classList.add('intro-appear');
+      icons.classList.add('intro-appear')
+      
+  }
+}
+
+window.addEventListener('scroll', scrollAppear);
+
+
+
+
+// MOBILE NAVIGATION //
+
+
 const nav = document.querySelector(".nav-links");
 const burger = document.querySelector(".burger");
 const links = nav.querySelectorAll("a");
@@ -15,6 +39,8 @@ links.forEach(link => {
 });
 
 
+//SLIDER ANIMATIONS - INTRO //
+
 const tl = gsap.timeline({defaults: {ease: "power1.out"}});
 tl.to('.text', {y:"0%", duration: 1.5, stagger: 0.75});
 tl.to('.text-2', {y:"0%", duration: 1.5, duration: 0.75});
@@ -22,9 +48,11 @@ tl.to('.text-3', {y:"0%", duration: 1.5, duration: 0.75});
 tl.to('.slider', {x: "-100%", duration: 1.5, delay: 0.5});
 tl.to('.intro', {x: "-100%", duration: 1},"-=1");
 tl.To('.hero-introduction', {opacity: 0}, {opacity: 1, duration: 1}, '-=1');
-tl.to('.media',  { opacity: 0},               { opacity: 1, duration: 1}, '-=1');
+tl.to('.media',  { opacity: 0}, { opacity: 1, duration: 1}, '-=1');
 
 //tl.to('.hero-introduction', {y: "25%", duration: 1},"-=.5");
+
+
 
 
 
